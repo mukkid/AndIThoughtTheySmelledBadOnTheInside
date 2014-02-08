6d02a6ab34e72f99ecc6d10541368420c8b74313
Mukund Kidambi <mukkid@gmail.com>
2014-02-08 16:24:19 -0600

    added weather grabbing

--- temperature.py
+++ temperature.py
@@ -8,21 +8,28 @@ url = 'http://www.weather.com/weather/today/Austin+TX+78705:4:US'
 def getTemperature():
 	s = requests.session()
 	soup = BeautifulSoup.BeautifulSoup(s.get(url).content)
+
+	#GET TEMPERATURE
 	temp = str(soup.find(itemprop="temperature-fahrenheit").string)
+	
+
 	inttemp = int(temp)
-	temp = "It is " + temp + " degrees outside."
+	temp = "It is " + temp + " degrees outside and "
+	#GET WEATHER
+	weather = str(soup.findAll('div', attrs={'class': 'wx-phrase '})[0].string) + ".  "
+	temp = temp + weather
+	
+
 	if inttemp <= 32:
 		temp = temp + "  It's pretty cold out there."
 	elif inttemp <= 50:
 		temp = temp + "  It's pretty chilly.  Wear a coat."
 	elif inttemp <= 70:
-		temp = temp + " It's pretty nice outside."
+		temp = temp + " It's alright, I suppose."
 	elif inttemp <= 90:
 		temp = temp + "  It's pretty hot out there."
 	else:
 		temp = temp + "  Jesus Christ it's way too hot out there."
-	print inttemp
-	print temp
 	return temp
 
 def sayIt(thing):
